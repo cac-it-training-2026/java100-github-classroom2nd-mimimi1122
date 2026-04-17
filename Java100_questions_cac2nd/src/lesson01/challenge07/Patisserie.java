a70a585798ae60203584b5ae66a434720085cb2b
mnamba <mnamba@C010423475.zipangu.cac.co.jp>
2026-04-17 16:08:47 +0900

    challenge0506

--- Java100_questions_cac2nd/src/lesson01/challenge07/Patisserie.java
+++ Java100_questions_cac2nd/src/lesson01/challenge07/Patisserie.java
@@ -41,6 +41,62 @@
 
 package lesson01.challenge07;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Patisserie {
+	public static void main(String[] args) throws IOException {
+		System.out.println("たいへんお待たせしました。");
+		System.out.println("【ポエール・ネルメ】");
+		System.out.println("ただいまより開店です!!");
+
+		double sit = 30;
+		double choko = 30;
+		double pis = 30;
+
+		System.out.println("\n本日のおすすめ商品です。\n");
+		System.out.println("シトロン      \\250 ・・・ 残り" + sit + "個");
+		System.out.println("ショコラ      \\280 ・・・ 残り" + choko + "個");
+		System.out.println("ピスターシュ  \\320 ・・・ 残り" + pis + "個");
+
+		System.out.println("\nそれぞれ何個ずつ買いますか？（最大30個まで）\n");
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.print("シトロン      >");
+		String sit2 = reader.readLine();
+		double sit3 = Double.parseDouble(sit2);
+
+		System.out.print("ショコラ      >");
+		String choko2 = reader.readLine();
+		double choko3 = Double.parseDouble(choko2);
+
+		System.out.print("ピスターシュ  >");
+		String pis2 = reader.readLine();
+		double pis3 = Double.parseDouble(pis2);
+
+		System.out.println("\nシトロン     " + sit3 + "個");
+		System.out.println("ショコラ     " + choko3 + "個");
+		System.out.println("ピスターシュ " + pis3 + "個");
+
+		double totalc = sit3 + choko3 + pis3;
+		double totalp = 250 * sit3 + 280 * choko3 + 320 * pis3;
+
+		System.out.println("\n合計個数     " + totalc + "個");
+		System.out.println("\n合計金額     " + totalp + "円");
+
+		System.out.println("\nをお買いあげですね。");
+		System.out.println("承りました。");
+
+		sit = sit - sit3;
+		choko = choko - choko3;
+		pis = pis - pis3;
+
+		System.out.println("本日のおすすめ商品です。\n");
+		System.out.println("シトロン   		\\250 ・・・ 残り" + sit + "個");
+		System.out.println("ショコラ   		\\280 ・・・ 残り" + choko + "個");
+		System.out.println("ピスターシュ   	\\320 ・・・ 残り" + pis + "個");
+	}
 
 }
