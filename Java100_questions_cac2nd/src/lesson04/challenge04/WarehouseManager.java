951516860d3597cf876ad5886d38846ade292a66
mnamba <mnamba@C010423475.zipangu.cac.co.jp>
2026-04-22 16:41:40 +0900

    lesson04,challenge04,05完成

--- Java100_questions_cac2nd/src/lesson04/challenge04/WarehouseManager.java
+++ Java100_questions_cac2nd/src/lesson04/challenge04/WarehouseManager.java
@@ -42,10 +42,13 @@ public class WarehouseManager {
 
 	public static void main(String[] args) {
 
-
 		//ここに配列の宣言および値の代入処理を記述する（for文）
+		int[] element = new int[5];
 
+		for (int i = 0; i < element.length; i++) {
+			element[i] = (int) (Math.random() * 5) + 1;
 
+		}
 		System.out.println("E主任：");
 		System.out.println("AB興産の荷物の検査結果を教えてください。\n");
 
@@ -54,10 +57,14 @@ public class WarehouseManager {
 
 		boolean hitFlag = false;
 
-
 		//ここに要素の確認および何袋目かの出力処理を記述する
-
-
+		for (int i = 0; i < element.length; i++) {
+			int Target = 5;
+			if (element[i] == Target) {
+				System.out.println((i + 1) + "袋目");
+				hitFlag = true;
+			}
+		}
 		if (hitFlag) {
 			System.out.println("に入っていました。");
 		} else {
